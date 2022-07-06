94f240b507bec161474391f3fab68518a0f8debd
Andre Possiedi <andreefps@gmail.com>
2022-07-06 21:46:39 +0200

    saving

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Video from "./components/Video";
 import Event from "./pages/Event";
+import Subscribe from "./pages/Subscribe";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<>OI NE</>} />
+      <Route path="/" element={<Subscribe />} />
       <Route path="/event" element={<Event />} />
       <Route path="/event/lesson/:slug" element={<Event />} />
     </Routes>
